40747274a08f34f6f62c0dd9cc5d6a7510389f97
Gaurav Kumar <gauravswarankar1@gmail.com>
2022-11-13 13:06:01 +0530

    urls added to html files

--- main.py
+++ main.py
@@ -45,7 +45,13 @@ def register():
     return render_template('register.html', title="Register", form=form)
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in', 'success')
+            return redirect(url_for("home"))
+        else:
+            flash('Login Unsuccessful Please Check Username and Password', "danger")
     return render_template('login.html', title="Login", form=form)

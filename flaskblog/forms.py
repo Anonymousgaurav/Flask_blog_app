2d5578c4b0651653685165210d5bdc76a7f4ccda
Gaurav Kumar <gauravswarankar1@gmail.com>
2022-12-11 14:28:59 +0530

    profile picture in account uploaded option added, update account details added

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,6 @@
+from flask_login import current_user
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
@@ -28,3 +30,22 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.userName:
+            user = User.query.filter_by(userName=username.data).first()
+            if user:
+                raise ValidationError('Username already exists !!')
+
+    def validate_email(self, email):
+        if email.data != current_user.userEmail:
+            user = User.query.filter_by(userEmail=email.data).first()
+            if user:
+                raise ValidationError('Email already exists !!')
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,8 +1,11 @@
+import os
+import secrets
+
 from flask import render_template, url_for, flash, redirect, request
 from flask_login import login_user, logout_user, current_user, login_required
 
 from flaskblog import bcrypt, db, app
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog.models import User
 
 userPosts = [
@@ -72,9 +75,32 @@ def logout():
     return redirect(url_for('login'))
 
 
-@app.route("/account")
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    # storing the user uploaded pics to package directory
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    form_picture.save(picture_path)
+    return picture_fn
+
+
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.userName = form.username.data
+        current_user.userEmail = form.email.data
+        db.session.commit()
+        flash('Your account has been successfully updated', "success")
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.userName
+        form.email.data = current_user.userEmail
+
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
-    print(image_file)
-    return render_template('account.html', title="Account", image_file=image_file)
+    return render_template('account.html', title="Account", image_file=image_file, form=form)

f273bbbec90d5ea2f76a59683dd0c298f27fd679
Gaurav Kumar <gauravswarankar1@gmail.com>
2022-12-11 22:46:15 +0530

    create_post implemented

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -6,29 +6,15 @@ from flask import render_template, url_for, flash, redirect, request
 from flask_login import login_user, logout_user, current_user, login_required
 
 from flaskblog import bcrypt, db, app
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
-from flaskblog.models import User
-
-userPosts = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    }
-]
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
+from flaskblog.models import User, Post
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts=userPosts)
+    posts = Post.query.all()
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
@@ -108,3 +94,16 @@ def account():
 
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title="Account", image_file=image_file, form=form)
+
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your Post has been posted !', "success")
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title="New Post", form=form)
